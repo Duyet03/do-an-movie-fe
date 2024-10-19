bb119ae22ef8c708056d2bb4c12740719327dd5e
Đại Official <dovandai2905@gmail.com>
2024-10-19 13:15:26 +0700

    fix menu rocker

--- src/layout/components/MenuRocker.vue
+++ src/layout/components/MenuRocker.vue
@@ -1,52 +1,41 @@
 <template>
-     <div class="nav primary-menu" style=" height: 40px">
+    <div class="nav-container primary-menu">
+        <div class="mobile-topbar-header">
+            <div>
+                <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+            </div>
+            <div>
+                <h4 class="logo-text">Rukada</h4>
+            </div>
+            <div class="toggle-icon ms-auto"><i class="bx bx-arrow-to-left"></i>
+            </div>
+        </div>
         <nav class="navbar navbar-expand-xl w-100">
-            <ul class="navbar-nav justify-content-center flex-grow-1 gap-1">
-                <li class="nav-item">
-                    <router-link to="/page1">
-                        <a class="nav-link" href="/page1">
-                            <div class="menu-title" style="font-size: 20px">
-                                SHOP QUÀ TẶNG
-                            </div>
-                        </a>
-                    </router-link>
+            <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
+                <li class="nav-item dropdown">
+                    <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret"
+                        data-bs-toggle="dropdown">
+                        <div class="parent-icon"><i class="bx bx-home-circle"></i>
+                        </div>
+                        <div class="menu-title">Dashboard</div>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li> <a class="dropdown-item" href="index.html"><i class="bx bx-right-arrow-alt"></i>Default</a>
+                        </li>
+                        <li> <a class="dropdown-item" href="index2.html"><i
+                                    class="bx bx-right-arrow-alt"></i>Alternate</a>
+                        </li>
+                        <li> <a class="dropdown-item" href="index3.html"><i
+                                    class="bx bx-right-arrow-alt"></i>Graphical</a>
+                        </li>
+                    </ul>
                 </li>
                 <li class="nav-item">
-                    <router-link to="/page1">
-                        <a class="nav-link" href="/page1">
-                            <div class="menu-title" style="font-size: 20px">MUA VÉ</div>
-                        </a>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/page1">
-                        <a class="nav-link" href="/page1">
-                            <div class="menu-title" style="font-size: 20px">PHIM</div>
-                        </a>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/page1">
-                        <a class="nav-link" href="/page1">
-                            <div class="menu-title" style="font-size: 20px">
-                                RẠP CHIẾU PHIM
-                            </div>
-                        </a>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/page1">
-                        <a class="nav-link" href="/page1">
-                            <div class="menu-title" style="font-size: 20px">KHUYẾN MÃI</div>
-                        </a>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/page1">
-                        <a class="nav-link" href="/page1">
-                            <div class="menu-title" style="font-size: 20px">LIÊN HỆ</div>
-                        </a>
-                    </router-link>
+                    <a class="nav-link" href="widgets.html">
+                        <div class="parent-icon"><i class="bx bx-cookie"></i>
+                        </div>
+                        <div class="menu-title">Widgets</div>
+                    </a>
                 </li>
             </ul>
         </nav>
