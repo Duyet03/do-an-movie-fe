c1f526ee9ca87823189e16f03c7dad6a8a3bf192
Minh Tùng Hồ <tungho.nth@gmail.com>
2024-10-31 19:44:24 +0700

    m

--- src/layout/components/BotRocker.vue
+++ src/layout/components/BotRocker.vue
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 <template>
     <footer class="page-footer">
         <p class="mb-0">Copyright © 2021. All right reserved.</p>
@@ -7,25 +5,7 @@
 </template>
 <script>
 export default {
-    
-}
-</script>
-<style>
-    
-</style>
-=======
 
-<template>
-    <footer class="page-footer">
-        <p class="mb-0">Copyright © 2021. All right reserved.</p>
-    </footer>
-</template>
-<script>
-export default {
-    
 }
 </script>
-<style>
-    
-</style>
->>>>>>> d762f9da3a502a557aa3634bca7f6cc74cacfdf1
+<style></style>
--- src/layout/wrapper/index.vue
+++ src/layout/wrapper/index.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
     <div class="wrapper">
         <div class="header-wrapper">
@@ -49,56 +48,4 @@ export default {
 @import "../../assets/css/header-colors.css";
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
-=======
-<template>
-    <div class="wrapper">
-        <div class="header-wrapper">
-            <TopRocker></TopRocker>
-            <MenuRocker></MenuRocker>
-        </div>
-        <div class="page-wrapper">
-            <div class="page-content">
-                <router-view></router-view>
-            </div>
-        </div>
-        <div class="overlay toggle-icon"></div>
-        <a href="javaScript:;" class="back-to-top">
-            <i class='bx bxs-up-arrow-alt'></i>
-        </a>
-        <BotRocker></BotRocker>
-    </div>
-</template>
-<script>
-import TopRocker from "../components/TopRoker.vue";
-import MenuRocker from "../components/MenuRocker.vue";
-import BotRocker from "../components/BotRocker.vue";
-import "../../assets/js/bootstrap.bundle.min.js";
-import "../../assets/js/jquery.min.js";
-import "../../assets/plugins/simplebar/js/simplebar.min.js";
-import "../../assets/plugins/metismenu/js/metisMenu.min.js";
-// import "../../assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js";
-import "../../assets/js/index.js";
-import "../../assets/js/app.js";
-export default {
-    name: "app",
-    components: {
-        TopRocker, MenuRocker, BotRocker
-    }
-}
-</script>
-<style>
-@import "../../assets/plugins/vectormap/jquery-jvectormap-2.0.2.css";
-@import "../../assets/plugins/simplebar/css/simplebar.css";
-/* @import "../../assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css"; */
-@import "../../assets/plugins/metismenu/css/metisMenu.min.css";
-@import "../../assets/css/bootstrap.min.css";
-@import "../../assets/css/bootstrap-extended.css";
-@import "../../assets/css/app.css";
-@import "../../assets/css/icons.css";
-@import "../../assets/css/dark-theme.css";
-@import "../../assets/css/semi-dark.css";
-@import "../../assets/css/header-colors.css";
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
-@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css');
->>>>>>> d762f9da3a502a557aa3634bca7f6cc74cacfdf1
 </style>
