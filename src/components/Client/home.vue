83cda202e3149b70a0eefb293d11cf2af4addd00
Minh Tùng Hồ <tungho.nth@gmail.com>
2024-11-02 01:57:00 +0700

    fix

--- src/components/Client/home.vue
+++ src/components/Client/home.vue
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 <template>
     <div class="container mt-5">
@@ -82,88 +82,4 @@ export default {
 };
 </script>
 <style></style>
-=======
 
-<template>
-    <div class="container mt-5">
-        <div class="row">
-            <template v-for="(value, index) in list_phim" :key="index">
-                <div class="col-lg-3 mb-2" style="border: 1px solid burlywood;padding-left: 0px;padding-right: 0px;">
-                    <router-link to="/view-sp2">
-                        <img class="img-fluid" :src="value.img" alt="" style="height: 480px;">
-                        <div class="mt-3 text-center">
-                            <h5 class="text-wrap">{{ value.name }}</h5>
-                            <hr style="margin-top: 7px;">
-                        </div>
-                        <div class="text-center" style="margin-top: 7px;">
-                            <div style="font-size: 20px;">{{ value.ngay }} | {{ value.tinh_trang }}</div>
-                        </div>
-                    </router-link>
-                </div>
-            </template>
-        </div>
-        <div class="text-center mt-3 mb-2" style="font-size: 20px">
-            <a href=""><B>XEM THÊM</B></a>
-        </div>
-    </div>
-</template>
-<script>
-export default {
-    data() {
-        return {
-            list_phim: [
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202409/11559_103_100001.jpg",
-                    name: "JUNG KOOK: I AM STILL",
-                    ngay: "16/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202410/11567_103_100006.jpg",
-                    name: "TEE YOD: QUỶ ĂN TẠNG PHẦN 2",
-                    ngay: "10/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202410/11482_103_100002.jpg",
-                    name: "ROBOT HOANG DÃ",
-                    ngay: "11/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202409/11507_103_100004.jpg",
-                    name: "CÁM",
-                    ngay: "10/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202410/11574_103_100002.jpg",
-                    name: "FUBAO: BẢO BỐI CỦA ÔNG",
-                    ngay: "18/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202410/11595_103_100001.jpg",
-                    name: "BIỆT ĐỘI HOT GIRL",
-                    ngay: "25/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202410/11592_103_100001.jpg",
-                    name: "TRÒ CHƠI NHÂN TÍNH",
-                    ngay: "25/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-                {
-                    img: "https://media.lottecinemavn.com/Media/MovieFile/MovieImg/202410/11556_103_100002.jpg",
-                    name: "CÔ DÂU HÀO MÔN",
-                    ngay: "18/10/2024",
-                    tinh_trang: "chưa mở",
-                },
-            ],
-        }
-    }
-};
-</script>
-<style></style>
->>>>>>> d762f9da3a502a557aa3634bca7f6cc74cacfdf1
